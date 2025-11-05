ab5752a472a89fada047484e7d5615073f1604e5
Braulio Tovar <bratoli1998@gmail.com>
2025-11-05 18:29:58 -0500

    Creación del Proyecto

---
+++ src/main/java/com/travel/ecoride_latam/EcoRideLatamApplication.java
@@ -0,0 +1,13 @@
+package com.travel.ecoride_latam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRideLatamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoRideLatamApplication.class, args);
+    }
+
+}
